f353167928c31fecc759b427668bc4aa3fb7b151
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-07-07 11:42:46 -0300

    integrando com API

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,54 @@
+import { useRef, useState, useEffect } from "react"
+import { NumeroResultado } from "./components/NumeroResultado"
+import axios from "axios"
 
 export function App() {
+
+  const megaSena = useRef('megasena')
+  const quina = useRef('quina')
+  const lotomania = useRef('lotomania')
+  const lotofacil = useRef('lotofacil')
+  const loteca = useRef('loteca')
+
+  const selectRef = useRef(null)
+
+  const [loteriaSelecionada, setLoteriaSelecionada] = useState('megasena')
+  const [dezenasLoteria, setDezenasLoteria] = useState([])
+
+  function selecionarLoteria () {
+    const valueSelect = selectRef.current.value
+    setLoteriaSelecionada(valueSelect)
+  }
+
+  useEffect(() => {
+
+    async function requisitarDadosNaAPI () {
+      const meuToken = 'T8EKhk8UaGklKsf'
+      const urlAPI = `https://apiloterias.com.br/app/resultado?loteria=${loteriaSelecionada}&token=${meuToken}`
+      const dados = await (await axios.get(urlAPI)).data
+      const dezenas = dados.dezenas
+      setDezenasLoteria(dezenas)
+    }
+    requisitarDadosNaAPI()
+
+    
+  }, [loteriaSelecionada])
+
 
   return (
     <>
 
+    <select onClick={selecionarLoteria} ref={selectRef}>
+      <option value={megaSena.current}>Mega-sena</option>
+      <option value={quina.current}>Quina</option>
+      <option value={lotomania.current}>Lotomania</option>
+      <option value={lotofacil.current}>Lotofácil</option>
+    </select>
 
-      <h1>oi</h1>
-      <h2>hi</h2>
-      
+    <h1>{loteriaSelecionada}</h1>
+
+    {dezenasLoteria.map((item) => <NumeroResultado numeroDezena={item} />)}
+ 
     </>
   )
 }
---
+++ src/components/NumeroResultado.tsx
@@ -0,0 +1,13 @@
+interface numeroDezena {
+  numeroDezena: number,
+}
+
+export function NumeroResultado ({numeroDezena}: numeroDezena) {
+
+
+  return (
+    <>
+      <h2>{numeroDezena}</h2>
+    </>
+  )
+}
